0a157fd6735542e255365f6002454506a68e0baf
Poojanv08 <68783693+Poojanv08@users.noreply.github.com>
2020-07-26 00:24:36 +0530

    Update Demo1.java

--- src/Design/Demo1.java
+++ src/Design/Demo1.java
@@ -4,9 +4,11 @@ public class Demo1
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("2");
+		System.out.println("1");
 		
 		System.out.println("2");
+		
+		System.out.println("3");
 
 	}
 
